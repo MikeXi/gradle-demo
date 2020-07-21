3a669995a42b8ad487df786f3ed4e5346fe0e791
Mike Xi <mike_xi@epam.com>
2020-07-21 15:19:00 +0800

    convert maven project to gradle project

---
+++ src/test/java/com/epam/helloci/AppTest.java
@@ -0,0 +1,40 @@
+package com.epam.helloci;
+
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ * @author vitali_shulha
+ */
+public class AppTest {
+
+	@Test
+	public void testShouldBePassed() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testShouldBeFailed() {
+		assertTrue(false);
+	}
+
+	@Ignore("Not implemented yet")
+	@Test
+	public void testShouldBeSKipped() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testAnotherMethod() {
+		App.doNothing();
+	}
+	
+	@Test
+	public void testAnotherClass() {
+		EmptyClass.emptyMethod();
+	}
+}
